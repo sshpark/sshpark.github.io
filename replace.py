1044af053e40e925d61bbfb7fbe6516383ed28dd
sshpark <huangjiamingwl@icloud.com>
2021-05-25 15:06:34 +0800

    Site updated: 2021-05-25 15:06:29

---
+++ replace.py
@@ -0,0 +1,14 @@
+import re
+import os
+
+g = os.walk('./_posts')
+
+for path,dir_list,file_list in g:
+    for file_name in file_list:
+        with open(os.path.join('./_posts',file_name), 'r+') as f:
+            content = f.read()
+            print(file_name, len(content))
+            content = content.replace(r'http://cdn.sshpark.com.cn/', '../images/')
+            f.seek(0)
+            f.truncate()
+            f.write(content)
