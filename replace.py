5442b64acd93cde1bc8baa5e95319f4617f5bace
mingohuang <mingohuang@tencent.com>
2021-12-22 15:47:21 +0800

    Site updated: 2021-12-22 15:47:19

--- replace.py
+++ replace.py
@@ -1,14 +1,17 @@
 import re
+import sys
 import os
 
-g = os.walk('./_posts')
+# g = os.walk('./_posts')
 
-for path,dir_list,file_list in g:
-    for file_name in file_list:
-        with open(os.path.join('./_posts',file_name), 'r+') as f:
-            content = f.read()
-            print(file_name, len(content))
-            content = re.sub('(?<=\().*?(?=/images)', 'http://sshpark.github.io', content)
-            f.seek(0)
-            f.truncate()
-            f.write(content)
+# for path,dir_list,file_list in g:
+    # for file_name in file_list:
+    #     with open(os.path.join('./_posts',file_name), 'r+') as f:
+file_name = sys.argv[1]
+with open(os.path.join(file_name), 'r+') as f:
+    content = f.read()
+    print(file_name, len(content))
+    content = re.sub('(?<=\().*?(?=/images)', 'http://sshpark.github.io', content)
+    f.seek(0)
+    f.truncate()
+    f.write(content)
